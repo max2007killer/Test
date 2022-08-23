df6d37ab47895a643e0f98372929942ce1a45d25
max2007killer <50517869+max2007killer@users.noreply.github.com>
2022-08-24 01:56:31 +0300

    Update lua.lua

--- lua.lua
+++ lua.lua
@@ -1,1 +1,8 @@
-'max2007killer','1Qi_0','Name','Name','Name','Name','Name','Name','Name','Name','Name','Name','Name','Name','Name','Name','Name','Name','Name','Name','Name','Name','Name','Name','Name','Name','Name','Name','Name','Name','Name'
+
+local player = game.Players.LocalPlayer 
+local AllowedPlayers = {'max2007killer','Name','Name'}
+
+for i = 1, #AllowedPlayers do
+  if player.Name == AllowedPlayers[i] then
+
+				
